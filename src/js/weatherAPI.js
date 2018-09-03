f78a7176b58a950e75c0a5005f12061f218529f4
lujanfernaud <lujanfernaud@gmail.com>
2018-09-03 11:42:17 +0100

    Switch OpenWeatherMap API for Weatherbit API

--- src/js/weatherAPI.js
+++ src/js/weatherAPI.js
@@ -1,19 +1,19 @@
 class WeatherAPI {
   constructor() {
-    this.appID = 'b25d1c2355e6eb105db6c4d083c30e51'
+    this.appID = '7e32e5caf8ee4462ae40a78352552372'
   }
 
   getData(city) {
-    const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.appID}&units=metric`
+    const apiURL = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${this.appID}`
 
     return fetch(apiURL, { mode: 'cors' })
       .then(response => response.json())
       .then(json => {
         return {
-          city: json.name,
-          status: json.weather[0].description,
-          icon: json.weather[0].icon,
-          temp: json.main.temp
+          city: json.data[0].city_name,
+          status: json.data[0].weather.description,
+          iconCode: json.data[0].weather.code,
+          temp: json.data[0].temp
         }
       })
       .then(cityData => {
