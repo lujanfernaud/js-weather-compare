6dac8b2bcec71a3919cc480c23ab36e524e7b7b3
lujanfernaud <lujanfernaud@gmail.com>
2018-09-04 13:31:56 +0100

    Refactor WeatherAPI.fetchData to use async/await

--- src/js/weatherAPI.js
+++ src/js/weatherAPI.js
@@ -3,22 +3,18 @@ class WeatherAPI {
     this.appID = '7e32e5caf8ee4462ae40a78352552372'
   }
 
-  fetchData(city) {
+  async fetchData(city) {
     const apiURL = `https://api.weatherbit.io/v2.0/current?city=${city}&key=${this.appID}`
 
-    return fetch(apiURL, { mode: 'cors' })
-      .then(response => response.json())
-      .then(json => {
-        return {
-          name: json.data[0].city_name,
-          status: json.data[0].weather.description,
-          iconCode: json.data[0].weather.code,
-          temp: json.data[0].temp
-        }
-      })
-      .then(cityData => {
-        return cityData
-      })
+    const response = await fetch(apiURL, { mode: 'cors' })
+    const json = await response.json()
+
+    return {
+      name: json.data[0].city_name,
+      status: json.data[0].weather.description,
+      iconCode: json.data[0].weather.code,
+      temp: json.data[0].temp
+    }
   }
 }
 
